4714303e707cfef4a2045f153538ab0c93bd9c4e
elneven <elise.neven@hotmail.be>
2023-05-02 12:49:26 +0300

    Test

--- Modele_exp.py
+++ Modele_exp.py
@@ -33,6 +33,8 @@ N_exp_RPM = N_exp*iNexp
 T_su_C = 76.85 #[°C]
 T_su = T_su_C+273.15 #[K]
 
+X=1
+
 #-------------------
 #Pas besoin askip
 # m_dot_r = 0.07 #[m/s]
@@ -49,6 +51,10 @@ P_ex = P_ex_bar*100000 #[Pa]
 # Known parameters
 V_s = 0.0000863 #[m^3/rev]
 rv_in = 2.3
+
+# Nominal conditions
+m_dot_n = 0.26
+N_n = 2000/60
 
 
 "1. Admission states: su"
@@ -86,20 +92,6 @@ v_su1 = 1./PropsSI('D', 'H', h_su1, 'P', P_su1, Fluid)
 T_su1 = PropsSI('T', 'H', h_su1, 'P', P_su1, Fluid)
 c_su1 = PropsSI('Cvmass', 'H', h_su1, 'P', P_su1, Fluid)
 
-# h_su1 = h_su #vanne isenthalpique
-# g = lambda variables: f(variables, s_su, m_dot_r, A_su, h_su)
-# solution = fsolve(g, [500000, 300000, 0.05])
-
-# "Calcul de h_thr, v_thr avec P_su1 -> PAS SUUUURE"
-
-# # P_thr_su = max(P_crit_su, P_su1) #PAS SUUUUUURE
-
-
-# # C_thr_ex = np.sqrt(2*(h_su-h_thr_su))
-
-# # h_su1 = h_thr_su + (C_thr_su**2)/2
-
-
 
 "3. Cooling at the entrance: su1->su2"
 #---------------------
@@ -107,7 +99,8 @@ m_dot = 0.102 #-> a enlever!!!
 #--------------------
 
 T_w = 58.121 #GUESS T at the fictious wall
-AU_su = 1 #GUESS
+AU_su_n = 1
+AU_su = AU_su_n*(m_dot/m_dot_n)**(0.8)
 C_dot_su = m_dot*c_su1
 NTU_su=AU_su/C_dot_su
 epsilon_su = 1-np.exp(-NTU_su)
@@ -174,7 +167,8 @@ c_ex1 = PropsSI('Cvmass', 'H', h_ex1, 'P', P_ex1, Fluid)
 
 "7. Heating at the exit: ex1->ex"
 
-AU_ex = 1 #GUESS
+AU_ex_n = 1 #-> GUESSSS
+AU_ex = AU_ex_n*(m_dot/m_dot_n)**(0.8)
 C_dot_ex = m_dot*c_ex1
 NTU_ex=AU_ex/C_dot_ex
 epsilon_ex = 1-np.exp(-NTU_ex)
@@ -188,9 +182,17 @@ s_ex = PropsSI('S', 'P', P_ex, 'H', h_ex, Fluid)
 
 "8. Fictious enveloppe: T_w"
 "Losses"
-T_loss = 3.5 #GUESSS
-W_dot_loss=2*np.pi*N*T_loss
- 
+# T_loss = 3.5 #GUESSS
+# W_dot_loss=2*np.pi*N*T_loss
+
+#---------------------
+#Autre def
+W_dot_loss_n = 1000
+alpha_loss = 0.3
+
+W_dot_loss = W_dot_loss_n*((N/N_n)**2) + alpha_loss * W_dot_in
+
+
 "Work at the shaft"
 # # W_dot_sh=2*np.pi*N*T -> besoin de T aussinon
 W_dot_sh = W_dot_in-W_dot_loss
@@ -218,17 +220,6 @@ T_w = Q_dot_amb/AU_amb + T_amb
 # Q_dot_amb=epsilon_ref*C_dot_ref*(T_w-T_su_ref)
 # epsilon_ref=1-exp(-NTU_ref)
 # NTU_ref=AU_ref/C_dot_ref
-
-
-
-
-
-
-
-
-
-
-
 
 
 
